4dab115fbe9a92ad8406bc38b174f22a0512486c
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-19 17:24:37 +0200

    jsnacks2

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+const nameList = [];
+
+let sum = 0;
+
+while( sum < 50 ){
+
+    let userNumber = parseInt(prompt('Insert a number'));
+
+    nameList.push(userNumber);
+
+    sum =+;
+
+};
+
+
+console.log(nameList);
