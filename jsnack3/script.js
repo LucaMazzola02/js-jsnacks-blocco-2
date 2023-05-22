5f08d83a6e210d302ea2be9d9c307cf0d2910c70
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-22 10:49:31 +0200

    add jsnack3

---
+++ jsnack3/script.js
@@ -0,0 +1,45 @@
+
+
+
+let sum = 0;
+
+for(let i = 0; i < 10 ; i++){
+
+    sum += i;
+
+}
+
+console.log(sum);
+
+console.log(sum / 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
