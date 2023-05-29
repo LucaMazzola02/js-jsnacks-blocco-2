ad01d8970c8e04b207e9aa65203fc5e8fc3a572b
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-29 11:47:33 +0200

    add all element of object

--- js-snack-oggetti/script.js
+++ js-snack-oggetti/script.js
@@ -2,15 +2,44 @@
 
 const basketPlayer = {
 
-    name : '',
-    surname : '',
-    age : '',
-    'point-average' : '',
-    'three-point-percentage' : '',
-    stoppet : '',
-    shoot : '',
+    'player-code':'',
+    name : 'Michael',
+    surname : 'Jordan',
+    age : 24,
+    'point-average' : 0,
+    'three-point-percentage' : 0,
+    stoppet : 0,
+    shoot : 0,
 }
 
+basketPlayer['player-code'] = generateRandomString(3,'QWERTYUIOPLKJHGFDSAZXCVBNM') + Math.floor(Math.random() * (9 - 1) + 1) + Math.floor(Math.random() * (9 - 1) + 1) + Math.floor(Math.random() * (9 - 1) + 1); 
+function generateRandomString(length, allowedChar) {
+    let result = '';
+    let counter = 0;
+
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+      counter++;
+    }
+
+    return result;
+}
+
+
+let average = basketPlayer['point-average'];
+average = Math.floor(Math.random() * (50 - 1) + 1);
+
+let threePointPercentage = basketPlayer['three-point-percentage'];
+threePointPercentage = Math.floor(Math.random() * (100 - 1) + 1);
+
+let stoppet = basketPlayer['stoppet'];
+stoppet = Math.floor(Math.random() * (30 - 1) + 1);
+
+let shoot = basketPlayer['shoot'];
+shoot = Math.floor(Math.random() * (100 - 20) + 20);
+
+
+console.log(basketPlayer);
 
 
 
